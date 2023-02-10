7ea398048bfe31f567c3e4a854ddc33c25028bc7
Skirmantas Auškalnis <79916263+vilktakis@users.noreply.github.com>
2023-02-10 10:57:22 +0200

    Add files via upload

---
+++ Security02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Security02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.ASCII;
+            Controler controler = new Controler();
+            controler.Start();
+        }
+    }
+}
